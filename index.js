72e5314ede2dff5cf9ed8f41068f92ab7b7dd6f1
mopahshewuri <mopahshewuri@yahoo.com>
2018-02-03 11:04:55 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+var form  = document.getElementsByTagName('form')[0];
+var email = document.getElementById('mail').value;
+var password = document.getElementById('psw').value;
+var error = document.querySelector('.error');
+
+
+
+form.addEventListener("submit", function (event) {
+  
+  event.preventDefault();
+  pass();
+
+});
+
+function pass()
+{
+    var passwordRegex = /^(?=.*\d)(?=.*[a-z])\w{6,}$/;
+    var emaiRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if(password == "" && email == "") 
+    {
+        error.innerHTML = "field can't be blank";
+        error.className = "error active";
+          return false;
+    }
+     else if(!passwordRegex.test(password)) {
+        error.innerHTML = "Passowrd must contain atleast a numbers!";
+          error.className = " error active";
+          return false;
+    }
+
+    else if(!emaiRegex.test(email))
+    {
+        error.innerHTML = "Invalid email";
+          error.className = " error active";
+          return false;
+    }
+    else
+    {
+        alert("Successful")
+    }
+
+
+}
